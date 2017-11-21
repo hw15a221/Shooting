415267d9691c3c772e8ab232593526058c503e6f
hw15a152 <33445426+hw15a152@users.noreply.github.com>
2017-11-21 14:43:09 +0900

    Update Game.cpp

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -73,7 +73,7 @@ void Update()
 
     // スコアの描画
     SetFont("nicoca_v1.ttf", 50.0f);
-    DrawText(FormatString("%03d", score), Vector2(-319, 199), Color::black);
-    DrawText(FormatString("%03d", score), Vector2(-320, 200), Color::white);
+    DrawText(FormatString("%05d", score), Vector2(-319, 199), Color::black);
+    DrawText(FormatString("%05d", score), Vector2(-320, 200), Color::white);
 }
 
